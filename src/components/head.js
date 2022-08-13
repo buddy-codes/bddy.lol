b31d58bce9677e85698d58494e95e7362cc21bca
voidx999 <55510116+voidx999@users.noreply.github.com>
2022-08-13 13:20:32 -0400

    💄 Added "General Sans" Font

--- src/components/head.js
+++ src/components/head.js
@@ -2,6 +2,10 @@ export default function Head() {
   return (
     <head>
       <link rel="stylesheet" href="" />
+      <link
+        href="https://api.fontshare.com/v2/css?f[]=general-sans@701,200,500,301,201,300,601,600,401,501,400,700&display=swap"
+        rel="stylesheet"
+      />
       <link rel="preconnect" href="https://fonts.googleapis.com/" />
       <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
       <link
