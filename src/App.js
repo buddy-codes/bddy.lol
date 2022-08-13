4bd4f233a0903fbe25aa9e1616239c38cc88d153
voidx999 <55510116+voidx999@users.noreply.github.com>
2022-08-13 11:47:01 -0400

    ✨ Meta Tags & Favicon

--- src/App.js
+++ src/App.js
@@ -11,15 +11,6 @@ function App() {
         href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
       />
-      <link rel="icon" type="image/png" href="../public/favicon.png" />
-
-      <meta property="og:title" content="bddy.lol" />
-      <meta
-        property="og:description"
-        content="hello people this is my personal website and its not finished this domain is pretty sick ngl anyways have a good plesent day k bye"
-      />
-      <meta property="og:image" content="../public/favicon.png" />
-      <meta property="twitter:card" content="summary_large_image" />
 
       <h2>bddy</h2>
       <p>
