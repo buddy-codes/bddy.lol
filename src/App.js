b1bff1c78aa8bac751bd592e42ae71729abe7e5b
voidx999 <55510116+voidx999@users.noreply.github.com>
2022-08-13 13:20:13 -0400

    ✨ Created Container

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ function App() {
   return (
     <div id="app">
       <Head />
-      <h2>bddy</h2>
-      <p>
-        hello people this is my personal website and its not finished this
-        domain is pretty sick ngl anyways have a good plesent day k bye
-      </p>
+      <div id="container">
+        <h2>bddy</h2>
+        <p>
+          Hey! I'm bddy. Also known as buddy. I'm a Full-Stack developer,
+          started off with{" "}
+          <a target="_blank" href="https://python.org">
+            Python
+          </a>{" "}
+          and{" "}
+          <a target="_blank" href="https://lua.org">
+            Lua
+          </a>
+        </p>
+        <a>Discord</a>
+      </div>
     </div>
   );
 }
