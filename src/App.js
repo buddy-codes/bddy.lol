4715d75ac6e2366b1620412f6fad0aa0e72ae568
voidx999 <55510116+voidx999@users.noreply.github.com>
2022-08-13 11:42:58 -0400

    🚀 Deploy v1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <link
+        rel="stylesheet"
+        href="https://css.furret.codes/furretcss/v2/full/light.min.css"
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com/" />
+      <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="icon" type="image/png" href="../public/favicon.png" />
+
+      <meta property="og:title" content="bddy.lol" />
+      <meta
+        property="og:description"
+        content="hello people this is my personal website and its not finished this domain is pretty sick ngl anyways have a good plesent day k bye"
+      />
+      <meta property="og:image" content="../public/favicon.png" />
+      <meta property="twitter:card" content="summary_large_image" />
+
+      <h2>bddy</h2>
+      <p>
+        hello people this is my personal website and its not finished this
+        domain is pretty sick ngl anyways have a good plesent day k bye
+      </p>
     </div>
   );
 }
