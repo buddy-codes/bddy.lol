09e2244714ab01dc8d3d13a8ac71d6b36e3eb163
voidx999 <55510116+voidx999@users.noreply.github.com>
2022-08-14 10:25:49 -0400

    💄 New Website Design

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import Head from "./components/head";
 
 function App() {
   return (
-    <div id="app">
+    <>
       <Head />
-      <div id="container">
-        <h2>bddy</h2>
-        <p>
-          Hey! I'm bddy. Also known as buddy. I'm a Full-Stack developer,
-          started off with{" "}
-          <a target="_blank" href="https://python.org">
-            Python
-          </a>{" "}
-          and{" "}
-          <a target="_blank" href="https://lua.org">
-            Lua
-          </a>
-        </p>
-        <a>Discord</a>
+      <nav>
+        <a href="#about">about</a>
+        <a href="#projects" style={{ textDecoration: "line-through" }}>projects</a>
+      </nav>
+      <div id="about" class="container">
+        <h1>bddy</h1>
+        <p>Full-Stack Developer // Efficient in Typescript, Javascript, HTML, CSS, and many more.</p>
+        {/* <span style={{
+          padding: "5px",
+          fontWeight: "200"
+        }}>Scroll for more.</span> */}
       </div>
-    </div>
+      {/* <div id="projects" class="containerauto">
+        <h1>Projects</h1>
+        <div id="wrapper">
+          <div id="project">
+            <h3>Replchat</h3>
+            <p>Another replit chat app that reached the peak of trending in April.</p>
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 }
 
