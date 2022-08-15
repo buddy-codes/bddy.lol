7caedcb2426fae72616bb9c905ee613de6c661cf
Avery <55510116+buddy-codes@users.noreply.github.com>
2022-08-14 21:50:00 -0400

    🔨 Fixed index.html, and renamed the app to JSX.

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,6 @@ function App() {
           fontWeight: "200"
         }}>Scroll for more.</span> */}
       </div>
-      {/* <div id="projects" class="containerauto">
-        <h1>Projects</h1>
-        <div id="wrapper">
-          <div id="project">
-            <h3>Replchat</h3>
-            <p>Another replit chat app that reached the peak of trending in April.</p>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
