b37802e6100c9962eb83b7f42e2791604f7eaae0
Avery <55510116+buddy-codes@users.noreply.github.com>
2022-09-08 10:38:40 +0000

    Projects, new background, fixed some css.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <Head />
       <nav>
         <a href="#about">about</a>
-        <a href="#projects" style={{ textDecoration: "line-through" }}>projects</a>
+				{/* <a href="#projects" style={{ textDecoration: "line-through" }}>projects</a> */}
       </nav>
       <div id="about" class="container">
         <h1>bddy</h1>
@@ -16,6 +16,32 @@ function App() {
           fontWeight: "200"
         }}>Scroll for more.</span> */}
       </div>
+			<div class="containerauto">
+				<div class="project" id="replchat">
+					<span>Replchat</span>
+					<p>Replchat was a #1 trending replit chat application</p>
+				</div>
+				<div class="project" id="2chat" onclick="location.href = '#2chat'">
+					<span>2chat / replrooms</span>
+					<p>2chat, also known as replrooms was ALSO a #1 replit chat application, but with voice chat. Was basically replchat remastered.</p>
+				</div>
+				<div class="project" id="bddylol" onclick="location.href = '#bddylol'">
+					<span>bddy.lol</span>
+					<p>bascially this website</p>
+				</div>
+				<div class="project" id="frail" onclick="location.href = '#frail'">
+					<span>Frail</span>
+					<p>Up and coming discord bot that's in the works.</p>
+				</div>
+				<div class="project" id="socialtime" onclick="location.href = '#socialtime'">
+					<span>SocialTime</span>
+					<p>Coming Soon</p>
+				</div>
+				<div class="project" id="...">
+					<span>And, many more to come!</span>
+					<p>...</p>
+				</div>
+			</div>
     </>
   );
 }
