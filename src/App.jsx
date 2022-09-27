4dba840428a132444b41a066e89e50fb779735ae
Avery <55510116+buddy-codes@users.noreply.github.com>
2022-09-27 22:41:03 +0000

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,44 @@
 import Head from "./components/head";
-import Project from "./components/project";
 
-const projects = [
-	{
-		"id": "replchat",
-		"name": "Replchat",
-		"description": "A chat hosted on replit.com. Replchat was a trending chat."
-	},
-	{
-		"id": "2chat",
-		"name": "2chat",
-		"description": "2chat was also a chat hosted on replit.com and was also trending! 2chat had the ability to voice chat with others."
-	},
-	{
-		"id": "bddylol",
-		"name": "bddy.lol",
-		"description": "what do you think it is lmao"
-	}
-]
 
 function App() {
   return (
     <>
       <Head />
-<main class="flex h-full w-full flex-col gap-2 overflow-auto bg-gray-900 p-5 text-white transition-all md:p-10 lg:p-14">
-  <span class="text-3xl font-semibold"> bddy </span>
-  <div class="flex h-full w-full flex-col md:flex-row gap-3 md:gap-2">
-    <div class="flex h-full md:w-1/2 w-full flex-col">
-      <div class="flex flex-col gap-2">
-			<span class="text-xl font-semibold">Hello, World!</span>
-      <p>I'm bddy. I enjoy programming and playing video games. I'm a Full-Stack Developer & Proficient in Typescript, Javascript, HTML, CSS, and many more languages!</p>
-			</div>
-      <div class="mt-2 flex flex-col gap-2">
-        <span class="text-xl font-semibold">Projects</span>
-				{projects.map(project => {
-					return <Project id={project.id} name={project.name} description={project.description} />
-				})}
+<div class="flex h-full w-full flex-col gap-5 bg-[#121212] p-5 md:p-14 lg:p-24 overflow-auto">
+  <div class="flex h-auto w-full flex-col items-center text-center md:text-left md:flex-row md:items-center gap-10 rounded-xl border border-zinc-700 bg-[#242424] p-9 shadow-lg">
+    <img src="https://bddy.lol/favicon.png" class="h-24 w-24 rounded-full" alt="" />
+    <div class="flex h-full w-full flex-col gap-2">
+      <span class="text-3xl font-semibold">bddy</span>
+      <div class="flex w-full flex-wrap gap-1 justify-center md:justify-start">
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">Full-Stack</div>
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">VSCode</div>
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">Replit</div>
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">GitHub</div>
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">Discord</div>
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">Slack</div>
       </div>
     </div>
-    <div class="flex h-full md:w-1/2 w-full flex-col">
-      <span class="text-xl font-semibold">Q&A (Not Done)</span>
-			{/*
-	 			<div class="flex-col mt-2">
-        <span class="text-lg">Question</span>
-        <p>Answer</p>
-      </div>
-			*/}
-		</div>
   </div>
-</main>
+
+  <div class="flex h-auto w-full flex-col gap-2 rounded-xl border border-zinc-700 bg-[#242424] p-9 shadow-lg">
+    <div class="text-2xl font-semibold">About Me</div>
+    <p>I'm bddy. I enjoy programming and playing video games. I'm a Full-Stack Developer & Proficient in Typescript, Javascript, HTML, CSS, and many more languages!</p>
+  </div>
+
+  <div class="flex h-auto w-full flex-col gap-3 rounded-xl border border-zinc-700 bg-[#242424] p-9 shadow-lg">
+    <div class="text-2xl font-semibold">Projects</div>
+    <div class="flex h-full w-full flex-col gap-2">
+      <div class="flex w-full flex-wrap gap-1">
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">Replchat</div>
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">2chat</div>
+        <div class="rounded-xl border border-zinc-700 bg-[#121212] px-5 py-2">bddy.lol</div>
+      </div>
+    </div>
+  </div>
+  <div class="w-full flex justify-end text-zinc-700">v3</div>
+</div>
+
 
     </>
   );
